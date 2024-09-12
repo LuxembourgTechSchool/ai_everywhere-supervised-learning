86b326fa2a07abba48816f7a5c159c4db285e443
FrauBoes <28864823+FrauBoes@users.noreply.github.com>
2024-09-12 11:50:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Module: AI Everywhere
+## Crash Course: Supervised Learning
+
+Files to follow along in the crash course. 
+
+## How to run the scripts
+1. Run `install.py`
+2. Run the notebook.
---
+++ install.py
@@ -0,0 +1,12 @@
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install("matplotlib")
+install("numpy")
+install("opencv-python")
+install("pandas")
+install("scikit-learn")
+install("tensorflow")
